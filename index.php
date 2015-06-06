a4939bf677453cf5970709e776844a4e30b8eda5
Sam Reed <reedy@wikimedia.org>
2015-06-07 00:40:26 +0100

    Add Last-Modified header

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+$headers = apache_request_headers();
+$mtime = filemtime( 'index.php' );
+$modifiedSince = isset( $headers['If-Modified-Since'] ) && ( strtotime( $headers['If-Modified-Since'] ) == $mtime );
+header( 'Last-Modified: ' . gmdate( 'D, d M Y H:i:s', $mtime ) . ' GMT', true, $modifiedSince ? 304 : 200 );
+?>
+
 <!DOCTYPE html>
 <html lang="en-GB" dir="ltr">
 <head>
