a6435114690c53c9b45cadd1689289334dca6126
Sam Reed <reedy@wikimedia.org>
2015-06-07 04:24:15 +0100

    Set Content-Type for api

--- api.php
+++ api.php
@@ -1,5 +1,6 @@
 <?php
 require_once( __DIR__ . '/functions.php' );
 lastModifiedHeaders();
+header( 'Content-Type: application/json' );
 echo json_encode( array ( 'isreedyintheuk' => isReedyInTheUK() ) );
 
