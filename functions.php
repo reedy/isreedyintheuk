d741c42694f7b2b95135fd6c8139cfade5e1ed1e
Sam Reed <sam@reedyboy.net>
2015-06-13 13:19:41 -0400

    Reformat/wrap long lines

--- functions.php
+++ functions.php
@@ -2,8 +2,13 @@
 function lastModifiedHeaders() {
 	$headers = apache_request_headers();
 	$mtime = filemtime( 'functions.php' );
-	$modifiedSince = isset( $headers['If-Modified-Since'] ) && ( strtotime( $headers['If-Modified-Since'] ) == $mtime );
-	header( 'Last-Modified: ' . gmdate( 'D, d M Y H:i:s', $mtime ) . ' GMT', true, $modifiedSince ? 304 : 200 );
+	$modifiedSince = isset( $headers['If-Modified-Since'] )
+		&& ( strtotime( $headers['If-Modified-Since'] ) == $mtime );
+	header(
+		'Last-Modified: ' . gmdate( 'D, d M Y H:i:s', $mtime ) . ' GMT',
+		true,
+		$modifiedSince ? 304 : 200
+	);
 }
 
 /**
