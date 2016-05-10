c69b7adf6ccec3bc0a13f78695fe20d78c1f7e49
Sam Reed <sam@reedyboy.net>
2016-05-10 18:22:11 +0100

    BRB

--- functions.php
+++ functions.php
@@ -31,6 +31,7 @@ function willReedyBeInTheUKAtThisTime( $time ) {
 		array( 'from' => '2015-09-03T14:30 +1:00', 'to' => '2015-09-30T13:35 +1:00' ),
 		array( 'from' => '2016-01-29T14:50 +1:00', 'to' => '2016-02-02T12:45 +1:00' ),
 		array( 'from' => '2016-03-31T12:55 +0:00', 'to' => '2016-04-05T00:35 +0:00' ),
+		array( 'from' => '2016-05-10T19:35 +1:00', 'to' => '2016-05-12T12:05 +1:00' ),
 	);
 	foreach( $timesNotInTheUK as $t ) {
 		if ( $time >= strtotime( $t['from'] ) && $time <= strtotime( $t['to'] ) ) {
